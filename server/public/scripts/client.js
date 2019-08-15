a2ddac5bae23af8928d6f0a7b7f307c3c7c204b8
Myron <dubtwee@gmail.com>
2019-08-15 16:27:26 -0500

    make an AJAX call to the new put endpoint for published date changes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,8 @@ function init() {
     // event listeners
     $('.s-add-song-btn').on('click', clickAddNewSong);
     $('.js-songs-list').on('click', '.js-btn-delete', clickDeleteSong);
+    $('.js-songs-list').on('click', '.js-btn-published-edit', clickPublishedEdit);
+    $('.js-songs-list').on('click', '.js-btn-published-save', clickPublishedSave);
 
     getSongs();
 }
@@ -32,6 +34,35 @@ function clickDeleteSong(event) {
     const songId = buttonDataObject.id;
 
     deleteSong(songId);
+}
+
+function clickPublishedEdit(event) {
+    const $parentElement = $(this).parent();
+    const $btnEdit = $parentElement.children('.js-btn-published-edit');
+    const $btnSave = $parentElement.children('.js-btn-published-save');
+    const $publishedText = $parentElement.children('.js-published-text');
+    const $publishedInput = $parentElement.children('.js-published-input');
+    const hideClass = 'hide';
+
+    // static ui not editable
+    $btnEdit.toggleClass(hideClass);
+    $publishedText.toggleClass(hideClass);
+    // interactive ui is editable
+    $btnSave.toggleClass(hideClass);
+    $publishedInput.toggleClass(hideClass);
+}
+
+function clickPublishedSave(event) {
+    const $parentElement = $(this).parent();
+    const $publishedInput = $parentElement.children('.js-published-input');
+    const publishedDate = $publishedInput.val();
+    const saveBtnData = $(this).data();
+    console.log($(this));
+    const songId = saveBtnData.id;
+
+    console.log('publishedDate: ', publishedDate);
+    console.log('songId: ', songId);
+    putSongPublished(publishedDate, songId);
 }
 
 //
@@ -81,6 +112,25 @@ function deleteSong(id) {
     });
 }
 
+function putSongPublished(publishedDate, songId) {
+    const serverPutObject = {
+        published: publishedDate,
+    };
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/songs/published/${songId}`,
+        data: serverPutObject,
+    })
+    .then(function(serverResponse) {
+        getSongs();
+    })
+    .catch(function(err) {
+        console.log('Error updating song: ', err);
+        alert('There was an Error updating your song.');
+    });
+}
+
 //
 // DOM / View rendering
 // ----------
@@ -100,7 +150,17 @@ function render (songsDataList) {
                 <div class="songPill">
                     ${song.track},
                     <p>by: ${song.artist}</p> 
-                    <p>published on: ${song.published}</p> 
+                    <div>
+                        published on: <span class="js-published-text">${song.published}</span>
+                        <input type="text" placeholder="Published" class="js-published-input hide" />
+                        <button class="js-btn-published-edit btn">Update</button>
+                        <button
+                            class="js-btn-published-save btn hide"
+                            data-id="${song.id}"
+                        >
+                            Save
+                        </button>
+                    </div> 
                     <button
                         class="js-btn-delete btn"
                         data-id="${song.id}"
